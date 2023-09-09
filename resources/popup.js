09171ed2dd73d6706377c1afa8b569029b1cd84f
tmbb-japan <woo787@naver.com>
2023-09-09 23:31:31 +0900

    refactor : 2023-09/09

--- resources/popup.js
+++ resources/popup.js
@@ -1,9 +1,12 @@
-document.getElementById('captureButton').addEventListener('click', () => {
-    chrome.tabs.captureVisibleTab({ format: 'png' }, (screenshotUrl) => {
-      const downloadLink = document.createElement('a');
-      downloadLink.href = screenshotUrl;
-      downloadLink.download = 'screenshot.png';
-      downloadLink.click();
+document.addEventListener('DOMContentLoaded', () => {
+    const captureButton = document.getElementById('captureButton');
+    captureButton.addEventListener('click', () => {
+      chrome.tabs.captureVisibleTab({ format: 'png' }, (screenshotUrl) => {
+        const downloadLink = document.createElement('a');
+        downloadLink.href = screenshotUrl;
+        downloadLink.download = 'screenshot.png';
+        downloadLink.click();
+      });
     });
-  });
+});
   
