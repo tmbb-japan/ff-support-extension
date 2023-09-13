b7a480a01dc93efe374ee4ef48c9cc2e687a9d94
tmbb-japan <woo787@naver.com>
2023-09-13 17:10:16 +0900

    refactor : 2023-09-13

--- resources/popup.js
+++ resources/popup.js
@@ -1,50 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const captureButton = document.getElementById('captureButton');
-
-  captureButton.addEventListener('click', async () => {
-    try {
-      const screenshotUrl = await captureScreenshot();
-      const imageBlob = await fetchImageBlob(screenshotUrl);
-      await uploadImage(imageBlob);
-      console.log('이미지 업로드 성공');
-    } catch (error) {
-      console.error('오류 발생:', error);
-    }
-  });
-
-  async function captureScreenshot() {
-    return new Promise((resolve, reject) => {
-      chrome.tabs.captureVisibleTab({ format: 'png' }, (screenshotUrl) => {
-        if (chrome.runtime.lastError) {
-          const errorMessage = chrome.runtime.lastError.message;
-          console.error('스크린샷 캡처 오류:', errorMessage);
-          reject(new Error(errorMessage));
-        } else {
-          resolve(screenshotUrl);
-        }
-      });
-    });
-  }
-
-  async function fetchImageBlob(url) {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('이미지를 가져오는 중 오류 발생');
-    }
-    return response.blob();
-  }
-
-  async function uploadImage(imageBlob) {
-    const formData = new FormData();
-    formData.append('imageData', imageBlob);
-
-    const response = await fetch('http://localhost:3000/upload', {
-      method: 'POST',
-      body: formData,
-    });
-
-    if (!response.ok) {
-      throw new Error('이미지 업로드 중 오류 발생');
-    }
-  }
+  document.getElementById('captureButton').addEventListener('click', captureAndUploadImage);
 });
+
+async function captureAndUploadImage() {
+  try {
+    const screenshotUrl = await captureScreenshot();
+    const imageBlob = await fetchImageBlob(screenshotUrl);
+    await uploadImage(imageBlob);
+    console.log('이미지 업로드 성공');
+  } catch (error) {
+    console.error('오류 발생:', error);
+  }
+}
+
+function captureScreenshot() {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.captureVisibleTab({ format: 'png' }, (screenshotUrl) => {
+      if (chrome.runtime.lastError) reject(new Error(chrome.runtime.lastError.message));
+      else resolve(screenshotUrl);
+    });
+  });
+}
+
+async function fetchImageBlob(url) {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error('이미지를 가져오는 중 오류 발생');
+  
+  return response.blob();
+}
+
+async function uploadImage(imageBlob) {
+  const formData = new FormData();
+  formData.append('imageData', imageBlob);
+
+  const response = await fetch('http://localhost:3000/upload', { method: 'POST', body: formData });
+  
+  if (!response.ok) throw new Error('이미지 업로드 중 오류 발생');
+}
