cac3a6e302c4286acd09ff224731f3e2bb1e1a86
tmbb-japan <woo787@naver.com>
2023-09-26 18:25:06 +0900

    scaffolding to vueJS

---
+++ ff-support-extension/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>웹 페이지 캡쳐</title>
+    </head>
+    <body>
+        <button id="captureButton">페이지 캡쳐</button>
+    </body>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+  }
+}
+</script>
