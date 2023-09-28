9cfb69651a074002b8306a944ea5896066433e00
tmbb-japan <woo787@naver.com>
2023-09-28 12:25:34 +0900

    refactor : viteJs

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const captureAndUploadImage = async () => {
+  try {
+    const screenshotUrl = await captureScreenshot();
+    const imageBlob = await fetchImageBlob(screenshotUrl);
+    await uploadImage(imageBlob);
+    console.log('이미지 업로드 성공');
+  } catch (error) {
+    console.error('오류 발생:', error);
+  }
+};
+
+const captureScreenshot = () => new Promise((resolve, reject) => {
+  chrome.tabs.captureVisibleTab({ format: 'png' }, (screenshotUrl) => {
+    if (chrome.runtime.lastError) reject(new Error(chrome.runtime.lastError.message));
+    else resolve(screenshotUrl);
+  });
+});
+
+const fetchImageBlob = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error('이미지를 가져오는 중 오류 발생');
+  return response.blob();
+};
+
+const uploadImage = async (imageBlob) => {
+  const formData = new FormData();
+  formData.append('imageData', imageBlob);
+
+  const response = await fetch('http://localhost:3000/upload', { method: 'POST', body: formData });
+
+  if (!response.ok) throw new Error('이미지 업로드 중 오류 발생');
+};
 </script>
+
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <button @click="captureAndUploadImage">페이지 캡쳐</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
